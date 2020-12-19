cb699530845dc53d3e480359287e867feb821f2b
shyjanharsha <shyjanharsha1998@gmail.com>
2020-12-19 14:36:00 +0530

    send link

--- public/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './app.css';
-import App from './forms.js';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
