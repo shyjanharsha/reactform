e54c8e82c22259177d63ae570e934b6965e53bbd
shyjanharsha <shyjanharsha1998@gmail.com>
2020-12-19 13:38:52 +0530

    send msg

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.css';
+import App from './forms.js';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
